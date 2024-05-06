0f51e01cd9ceabfe4ed0ea255bf621e02640ca96
Travik <travikdev@gmail.com>
2024-05-07 00:57:27 +0300

    upd: nav item fade-in-out hover animation

--- app/navItem.tsx
+++ app/navItem.tsx
@@ -10,16 +10,16 @@ export default function NavItem({ item }: PropsNav) {
   const pathname = usePathname();
 
   return (
-    <li
-      className={twMerge(
-        rubik600.className,
-        "px-4 py-3 text-[16px] font-bold leading-[21.28px]",
-        pathname === item.url ? "bg-submenu-active" : "bg-submenu-inactive",
-      )}
-    >
-      <Link href={item.url}>
+    <Link href={item.url}>
+      <li
+        className={twMerge(
+          rubik600.className,
+          "px-4 py-3 text-[16px] font-bold leading-[21.28px] transition ease-in hover:bg-background-details-active",
+          pathname === item.url ? "bg-submenu-active" : "bg-submenu-inactive",
+        )}
+      >
         <p className="h-[21px] w-[199px]">{item.title}</p>
-      </Link>
-    </li>
+      </li>
+    </Link>
   );
 }
