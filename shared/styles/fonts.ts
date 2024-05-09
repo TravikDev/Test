ca1a1f7b90dfe57667529dc6732a8c8996c2b068
Travik <travikdev@gmail.com>
2024-05-10 01:41:37 +0300

    add: fonts -> adjustFontFallback

--- shared/styles/fonts.ts
+++ shared/styles/fonts.ts
@@ -3,10 +3,19 @@ import { Rubik, Sofia_Sans } from "next/font/google";
 const rubik600 = Rubik({
   subsets: ["latin"],
   weight: "600",
+  adjustFontFallback: false,
   // weight: ["600", "400"],
   // style: ["normal", "italic"],
-  //   preload: tru[e
+  //   preload: true
 });
-const rubik400 = Rubik({ subsets: ["latin"], weight: "400" });
-const sofiaSans = Sofia_Sans({ subsets: ["latin"], weight: "800" });
+const rubik400 = Rubik({
+  subsets: ["latin"],
+  weight: "400",
+  adjustFontFallback: false,
+});
+const sofiaSans = Sofia_Sans({
+  subsets: ["latin"],
+  weight: "800",
+  adjustFontFallback: false,
+});
 export { rubik400, rubik600, sofiaSans };
